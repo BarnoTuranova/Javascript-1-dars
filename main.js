00a9d46a9ff709a3899799c3f7224d8bd86e30cc
BarnoTuranova <138581787+BarnoTuranova@users.noreply.github.com>
2023-07-30 16:17:57 +0500

    Javascript 1-dars
    24-guruh o'quvchisi

---
+++ main.js
@@ -0,0 +1,34 @@
+if (20 == 16){
+    console.log("Ikki so'z to'gri yozilgan!")
+}
+else{
+    console.log("Ikkalasidagi qiymat teng emas!")
+}
+
+if (78 == 80){
+    console.log("Teng!")
+}
+else{
+    console.log("Teng emas")
+}
+
+if (50 >= 100){
+    console.log("Qiymatlar teng!")
+}
+else{
+    console.log("Qiymatlarning biri ikkinchidan katta!")
+}
+
+if (58>=30){
+    console.log("Qiymat darajasi teng!")
+}
+else{
+    console.log("Qiymat darajasi kam")
+}
+
+if (16 <= 30){
+    console.log("Sonlar o'zaro teng!")
+}
+else{
+    console.log("Sonlar teng emas!")
+}
---
+++ main1.js
@@ -0,0 +1,50 @@
+if (10 == 16){
+    console.log("Ikki soon ajoyib yozilgan!")
+}
+else if (10 == 18){
+    console.log("Bu qiymat aniq teng!")
+}
+
+else{
+    console.log("Qiymat teng emas!")
+}
+
+if (78 == 78){
+    console.log("Teng!")
+}
+else if (78==80){
+console.log(" Qiymat teng!")
+}
+else{
+    console.log("Teng emas")
+}
+
+if (10 >= 100){
+    console.log("Qiymatlar teng!")
+}
+else if ( 10 == 100){
+    console.log("Qiymatlar barqaror!")
+}
+else{
+    console.log("Qiymatlarning biri ikkinchidan katta!")
+}
+
+if (56>=10){
+    console.log("Qiymat darajasi teng!")
+} 
+else if(56==56){
+    console.log("Bir-biriga teng!")
+}
+
+else{
+    console.log("Qiymat darajasi kam")
+}
+if (12 == 12){
+    console.log("Sonlar o'zaro teng!")
+}
+else if (500==500){
+    console.log("Sonlar aqlbovar qilib qolmaydi!")
+}
+else{
+    console.log("Sonlar teng emas!")
+}
